21eba2ba4a75e8258dcd9ff41f930ad92077c778
lmachens <leon@machens.koeln>
2021-10-11 17:42:03 +0200

    Alert if answer is correct

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+const correctAnswer = false;
+
 const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Are you a robot 🤖?";
+
+const yesButton = document.querySelector(".yes");
+yesButton.onclick = function () {
+  if (correctAnswer === true) {
+    alert("This is correct 😁");
+  } else {
+    alert("This is incorrect 😭");
+  }
+};
+
+const noButton = document.querySelector(".no");
+noButton.onclick = function () {
+  if (correctAnswer === false) {
+    alert("This is correct 😁");
+  } else {
+    alert("This is incorrect 😭");
+  }
+};
